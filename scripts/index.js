041164abe3626fef5bc3099dc9d5880a2da4d46d
Lotechi Ozomah <55860602+lotechiozomah@users.noreply.github.com>
2022-05-26 14:22:30 -0400

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body")
+const navbar = document.querySelector(".header")
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabledScroll");
+
+}
+cancelBtn.onclick = ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    body.classList.remove("disabledScroll");
+}
+
+window.onscroll = ()=>{
+    this.scrollY > 20 ? header.classList.add("sticky") : header.classList.remove("sticky"); 
+}
